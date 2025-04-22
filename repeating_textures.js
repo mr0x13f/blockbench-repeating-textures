28e961f47666fa71b85e3d5cdb727858d6ec21d8
0x13f <22893194+mr0x13f@users.noreply.github.com>
2025-04-22 17:00:21 +0200

    Create repeating_textures.js

---
+++ repeating_textures.js
@@ -0,0 +1,43 @@
+(function() {
+
+let deferred = [];
+
+Plugin.register('repeating_textures', {
+    title: 'Repeating Textures',
+    description: 'Fixes repeating textures',
+    icon: 'qr_code',
+    onload() {
+
+        let OriginalTexture = Texture;
+        class TweakedTexture extends OriginalTexture {
+            constructor() {
+                super()
+                this.img.tex.magFilter = THREE.NearestFilter
+                this.img.tex.minFilter = THREE.NearestFilter
+                this.img.tex.wrapS = THREE.RepeatWrapping;
+                this.img.tex.wrapT = THREE.RepeatWrapping;
+            }
+        }
+        Texture = TweakedTexture;
+        window.Texture = TweakedTexture;
+        Blockbench.Texture = TweakedTexture;
+        defer(() => {
+            Texture = OriginalTexture;
+            window.Texture = OriginalTexture;
+            Blockbench.Texture = OriginalTexture;
+        });
+
+    },
+    onunload() {
+
+        for (let lambda of deferred)
+            lambda();
+        
+    },
+});
+
+function defer(lambda) {
+    deferred.push(lambda);
+}
+
+})();
